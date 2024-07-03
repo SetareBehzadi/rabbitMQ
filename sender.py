d2c77ef623bc750818e3bd2a205cdad26f1af724
Setare <setare.behzadi@gmail.com>
2024-07-03 14:28:41 +0330

    properties

--- sender.py
+++ sender.py
@@ -1,3 +1,5 @@
+from datetime import time
+
 import pika
 
 credential = pika.PlainCredentials('setare', 'setare')
@@ -5,6 +7,16 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',
 chanel = connection.channel()
 
 chanel.queue_declare('one')
-chanel.basic_publish('', 'one','Bye rabbit')
+chanel.basic_publish('', 'one','Bye rabbit',
+                     properties=pika.BasicProperties(
+                        content_type='text/plain',
+                         content_encoding='gzip',
+                         timestamp=100000000,
+                         delivery_mode=2,
+                         user_id="setare",
+                         type="exchange.queue",
+                         headers={"name":'amir', 'age':30}
+                     )
+                     )
 print('message sent')
 chanel.close()
