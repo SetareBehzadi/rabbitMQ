d25e1da973708b0bb305c6f5cf1ba043e5eb12cb
Setare <setare.behzadi@gmail.com>
2024-07-06 09:44:46 +0330

    acknowledge

--- receiver.py
+++ receiver.py
@@ -1,3 +1,5 @@
+import time
+
 import pika
 
 
@@ -9,8 +11,14 @@ ch.queue_declare('one')
 
 def callback(ch, method, properties, body):
     print(f'Received message {body}')
+    print(method)
+    time.sleep(5)
+    print( properties.type)
+    print('done')
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-ch.basic_consume(queue='one', on_message_callback=callback, auto_ack=True)
+ch.basic_qos(prefetch_count=1)
+ch.basic_consume(queue='one', on_message_callback=callback)
 print("Waiting for message, to exit press ctrl+c")
 ch.start_consuming()
