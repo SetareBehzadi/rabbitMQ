ea38ecf952558dfc0273e75ca4a2eded19c41e74
Setare <setare.behzadi@gmail.com>
2024-07-08 11:46:29 +0330

    basic_nack for deadletter

--- client.py
+++ client.py
@@ -3,9 +3,9 @@ import pika
 credentials = pika.PlainCredentials('setare','setare')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1',credentials=credentials))
 ch = connection.channel()
+ch.exchange_declare('acceptReject', exchange_type='fanout')
 
-ch.exchange_declare(exchange='mains', exchange_type='direct')
-
-ch.basic_publish(exchange='mains', routing_key='home', body='Hello World')
-print('Sent...!')
-connection.close()
+while True:
+    ch.basic_publish(exchange='acceptReject',routing_key='home', body='Hello world...')
+    print('sent...')
+    input('Press any key to continue...')
--- server.py
+++ server.py
@@ -4,22 +4,17 @@ credentials = pika.PlainCredentials('setare','setare')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1',credentials=credentials))
 ch = connection.channel()
 
-ch.exchange_declare(exchange='mains', exchange_type='direct')
-ch.exchange_declare(exchange='delx', exchange_type='fanout')
+ch.exchange_declare('acceptReject', exchange_type='fanout')
+ch.queue_declare(queue='nackq')
 
-ch.queue_declare(queue='mainqq', arguments={'x-dead-letter-exchange': 'delx', 'x-message-ttl': 5000, "x-max-length": 10})
+ch.queue_bind('nackq', 'acceptReject')
 
-ch.queue_bind('mainqq', 'mains', 'home')
+def callback(ch, method, properties, body):
+    if method.delivery_tag % 5 == 0:
+        ch.basic_nack(delivery_tag=method.delivery_tag, multiple=True, requeue=False)
+    print(f'get Request: {method.delivery_tag}')
 
-ch.queue_declare(queue='delxq')
-ch.queue_bind('delxq', 'delx')
 
-def delx_callback(ch, method, properties, body):
-    print(f'delx call: {body}')
-
-#
-# ch.basic_consume('mainq', on_message_callback=main_callback, auto_ack=True)
-ch.basic_consume('delxq', on_message_callback=delx_callback, auto_ack=True)
-
-print('Start consuming.')
+ch.basic_consume(queue='nackq', on_message_callback=callback)
+print('start consume')
 ch.start_consuming()
