c35dbf6ef84beaffebb6fc59d61bf86bd4ee882c
Setare <setare.behzadi@gmail.com>
2024-07-09 10:59:28 +0330

    confirm

--- client.py
+++ client.py
@@ -1,11 +1,28 @@
+import time
+
 import pika
 
-credentials = pika.PlainCredentials('setare','setare')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1',credentials=credentials))
+credentials = pika.PlainCredentials('setare', 'setare')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1', credentials=credentials))
 ch = connection.channel()
-ch.exchange_declare('acceptReject', exchange_type='fanout')
+ch.exchange_declare('publisherConfirm', exchange_type='direct', auto_delete=False, durable=True)
+ch.queue_declare('mainqqw', exclusive=False, auto_delete=False, durable=True)
+
+ch.queue_bind('mainqqw', 'publisherConfirm', 'home')
+ch.confirm_delivery()
+
+for i in range(20):
+    try:
+        ch.basic_publish('publisherConfirm', 'home', body='Hello', properties=pika.BasicProperties(
+            content_type='text/plain',
+            delivery_mode=2),
+                         mandatory=True)
+        print(f'message: {i}')
+    except Exception as e:
+        print(f'Exception:{type(e).__name__}')
+    time.sleep(2)
 
 while True:
-    ch.basic_publish(exchange='acceptReject',routing_key='home', body='Hello world...')
+    ch.basic_publish(exchange='acceptReject', routing_key='home', body='Hello world...')
     print('sent...')
     input('Press any key to continue...')
--- server.py
+++ server.py
@@ -4,17 +4,15 @@ credentials = pika.PlainCredentials('setare','setare')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1',credentials=credentials))
 ch = connection.channel()
 
-ch.exchange_declare('acceptReject', exchange_type='fanout')
-ch.queue_declare(queue='nackq')
+ch.exchange_declare('publisherConfirm', exchange_type='direct', auto_delete=False, durable=True)
+ch.queue_declare('mainqqw', exclusive=False, auto_delete=False, durable=True)
 
-ch.queue_bind('nackq', 'acceptReject')
+ch.queue_bind('mainqqw','publisherConfirm', 'home')
 
 def callback(ch, method, properties, body):
-    if method.delivery_tag % 5 == 0:
-        ch.basic_nack(delivery_tag=method.delivery_tag, multiple=True, requeue=False)
-    print(f'get Request: {method.delivery_tag}')
+    print(f'get Request: {body}')
 
 
-ch.basic_consume(queue='nackq', on_message_callback=callback)
+ch.basic_consume(queue='mainqqw', on_message_callback=callback)
 print('start consume')
 ch.start_consuming()
